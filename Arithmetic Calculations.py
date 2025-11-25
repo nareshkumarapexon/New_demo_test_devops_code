6aa6956ba46dd8b32c02b912241900dd21203879
nareshkumarapexon <naresh.mathamsetty@apexon>
2025-11-25 19:09:19 +0530

    Comment

--- Arithmetic Calculations.py
+++ Arithmetic Calculations.py
@@ -18,7 +18,7 @@ a=int(input("Enter the value a = "))
 b=int(input("Enter the value b = "))
 print("\nKindly enter only below options")
 print("\n 1 = Addition \n 2 = Subtraction \n 3 = Multiplication \n 4 = Division")
-s= int(input("\n Enter the Arithmetic opertion: " ))
+s= int(input("\n Enter the Arithmetic Opertion: " ))
 
 if s==1:
     print(f"Result:{a}+{b} =",sum_it(a,b))
@@ -29,4 +29,4 @@ elif s==3:
 elif s==4:
     print(f"Result:{a}/{b} =",division(a,b))
 else:
-    print("Enter only assgined number for Arithmetic opertion that mentioned above:-).")
+    print("Enter only assigned number for Arithmetic Opertions which are mentioned above:-).")
